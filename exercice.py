427e883785668f53ea2256e08fc346f7b36b35b4
GabSawka <gabriel.sawka@polymtl.ca>
2021-09-13 13:49:24 -0400

    v1

--- exercice.py
+++ exercice.py
@@ -2,24 +2,49 @@
 # -*- coding: utf-8 -*-
 
 
+from unittest.signals import removeResult
+
+
 def is_even_len(string: str) -> bool:
-    return False
+    return len(string) % 2 == 0
 
 
 def remove_third_char(string: str) -> str:
-    return ""
-
-
+    if(len(string)>=3):
+        new_string =""
+        for i in range(len(string)):
+            if i != 3:
+                new_string+=string[i]
+        return new_string
+    else:
+        return string
+        
 def replace_char(string: str, old_char: str, new_char: str) -> str:
-    return ""
+    new_string =""
+    for letter in string:
+        if letter == old_char:
+            new_string+=new_char
+        else:
+            new_string+=letter
+    return new_string
 
 
 def get_nb_char(string: str, char: str) -> int:
-    return 0
+    char_appearance=0
+    for letter in string:
+        if letter == char:
+            char_appearance+=1
+    return char_appearance
 
 
 def get_nb_words(sentence: str) -> int:
-    return 0
+    if len(sentence)==0:
+        return 0
+    nb_words=1
+    for letter in sentence:
+        if letter == ' ':
+            nb_words+=1
+    return nb_words
 
 
 def main() -> None:
@@ -32,7 +57,7 @@ def main() -> None:
 
     string = "hello world!"
     print(f"On remplace le caratère w par le caractère z dans la chaine: '{string}'. Résultat: {replace_char(string, 'w', 'z')}")
-
+    string = "hello"
     print(f"Le nombre d'occurrence de l dans hello est : {get_nb_char(string, 'l')}")
     
     string = "Baby shark doo doo doo doo doo doo"
